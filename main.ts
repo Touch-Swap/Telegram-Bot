7565b12b9e0780f95d83677be768ef746280ca74
Angry Coding <mailidanielifechukwu@gmail.com>
2024-04-26 00:35:21 +0100

    Changes

--- main.ts
+++ main.ts
@@ -1,31 +1,31 @@
 import { onShutdown } from "node-graceful-shutdown";
-import  createBot  from "./bot";
-import  config  from "./config";
-import  logger  from "./logger";
+import createBot from "./bot";
+import config from "./config";
+import logger from "./logger";
 import { createServer } from "./server";
 
 async function runBot() {
-    try {
+  try {
     const bot = createBot(config.BOT_TOKEN);
     const server = await createServer(bot);
-  
+
     // Graceful shutdown
     onShutdown(async () => {
       logger.info("shutdown");
-  
+
       await server.close();
       await bot.stop();
     });
-  
+
     if (config.BOT_MODE === "webhook") {
       // to prevent receiving updates before the bot is ready
       await bot.init();
-  
+
       await server.listen({
         host: config.BOT_SERVER_HOST,
         port: config.BOT_SERVER_PORT,
       });
-  
+
       await bot.api.setWebhook(config.BOT_WEBHOOK, {
         allowed_updates: config.BOT_ALLOWED_UPDATES,
       });
@@ -46,12 +46,12 @@ async function runBot() {
 }
 
 async function startBot() {
-    try {
-      const result = await runBot();
-      console.log(result); // Log the result on successful execution
-    } catch (error) {
-      console.error(error); // Handle any errors that occur
-    }
+  try {
+    const result = await runBot();
+    console.log(result); // Log the result on successful execution
+  } catch (error) {
+    console.error(error); // Handle any errors that occur
+  }
 }
 
-  startBot()
+startBot();
