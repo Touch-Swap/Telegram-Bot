8917395819aabbadca4c541f8b7c93b285918d74
Angry Coding <mailidanielifechukwu@gmail.com>
2024-04-25 22:56:47 +0100

    Middleware , Looger and Helpres

---
+++ middleware/update-logger.ts
@@ -0,0 +1,34 @@
+import { performance } from "node:perf_hooks";
+import { Middleware } from "grammy";
+import type { Context } from "../contexts";
+import { getUpdateInfo } from "../helpers";
+
+export function updateLogger(): Middleware<Context> {
+  return async (ctx, next) => {
+    ctx.api.config.use((previous: (arg0: any, arg1: any, arg2: any) => any, method: any, payload: any, signal: any) => {
+      ctx.logger.debug({
+        msg: "bot api call",
+        method,
+        payload,
+      });
+
+      return previous(method, payload, signal);
+    });
+
+    ctx.logger.debug({
+      msg: "update received",
+      update: getUpdateInfo(ctx),
+    });
+
+    const startTime = performance.now();
+    try {
+      await next();
+    } finally {
+      const endTime = performance.now();
+      ctx.logger.debug({
+        msg: "update processed",
+        duration: endTime - startTime,
+      });
+    }
+  };
+}
